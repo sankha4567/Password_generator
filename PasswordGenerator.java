28107fc86f527a24ba9632417bab12b813d8d2ae
sankha4567 <118678008+sankha4567@users.noreply.github.com>
2024-07-13 18:35:48 +0530

    Add files via upload

---
+++ PasswordGenerator.java
@@ -0,0 +1,31 @@
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+
+    private static final String CHAR_LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String CHAR_UPPER = CHAR_LOWER.toUpperCase();
+    private static final String NUMBER = "0123456789";
+    private static final String OTHER_CHAR = "!@#$%&*()_+-=[]|,./?><";
+
+    private static final String PASSWORD_ALLOW_BASE = CHAR_LOWER + CHAR_UPPER + NUMBER + OTHER_CHAR;
+    private static SecureRandom random = new SecureRandom();
+
+    public static void main(String[] args) {
+        int length = 12; // Password length
+        System.out.println(generatePassword(length));
+    }
+
+    public static String generatePassword(int length) {
+        if (length < 8) {
+            throw new IllegalArgumentException("Password length must be at least 8 characters");
+        }
+
+        StringBuilder password = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(PASSWORD_ALLOW_BASE.length());
+            password.append(PASSWORD_ALLOW_BASE.charAt(randomIndex));
+        }
+
+        return password.toString();
+    }
+}
